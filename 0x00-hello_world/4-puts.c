073fee3c0d87d3f62143190a863cc4ab601f03ee
DzidediSenaya <dzidedisenaya@gmail.com>
2023-02-16 12:35:52 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints out string using puts function.
+ * Return: 0 if always.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
