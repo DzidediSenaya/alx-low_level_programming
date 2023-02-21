93686e6fec856356c698cd8613f68e83a63de1fa
DzidediSenaya <dzidedisenaya@gmail.com>
2023-02-21 13:47:58 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - Make alphabet x10 times
+  *
+  * Return: void
+  */
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+	for (a = 1; 'a' <= 10; a++)	
+	for (b = 'a'; 'b' <= 'z'; b++)
+
+	_putchar(b);
+	_putchar('\n');	
+}
