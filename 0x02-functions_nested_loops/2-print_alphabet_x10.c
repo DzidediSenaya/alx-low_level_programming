2a7dbf17deb9976658e675c2638b2f7a03e7ffc0
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-02-21 21:52:15 +0000

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 {
 	int a;
 	char b;
-
-	for (a = 1; 'a' <= 10; a++)	
+	
+	for (a = 1; 'a' <= 10; a++)
 	for (b = 'a'; 'b' <= 'z'; b++)
 
 	_putchar(b);
-	_putchar('\n');	
+	_putchar('\n');
 }
