a89044f4aaa997391c22dfa263884aab04c1c651
DzidediSenaya <dzidedisenaya@gmail.com>
2023-02-21 19:09:32 -0800

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,49 @@
 #include "main.h"
 
 /**
- *print_times_table - print time table.
- * @n: integer
- *Return:void
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
  */
 void print_times_table(int n)
 {
-	int a, b, c;
+	int x = 0, y, z;
 
-	if (n >= 0 && n <= 15)
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (a = 0; a <= n; a++)
+		for (y = 0; y <= n; y++)
 		{
-			_putchar('0');
-			if (n != 0)
-				_putchar(',');
-			for (b = 1; b <= n; b++)
+			z = x * y;
+			if (z > 99)
 			{
-				c = a * b;
-				if (c < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + c);
-				}
-				else if (c < 100)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
-				}
-				else
-				{
-					_putchar(' ');
-					_putchar((c / 100) + '0');
-					_putchar(((c % 100) / 10) + '0');
-					_putchar(((c % 100) % 10) + '0');
-				}
-				if (a < n)
-					_putchar(',');
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			_putchar('\n');
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
+		x++;
 	}
 }
