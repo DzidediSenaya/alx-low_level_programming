3ac86170f6d6a074e334e50dc751f83766f3aca3
DzidediSenaya <dzidedisenaya@gmail.com>
2023-02-23 16:07:18 -0800

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,8 @@ void more_numbers(void)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			if (b > 10)
-				_putchar((b / 10) + '0');
+			if (b >= 10)
+				_putchar(('1');
 			_putchar((b % 10) + '0');
 		}
 	_putchar('\n');
