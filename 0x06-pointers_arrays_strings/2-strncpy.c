c47f73a8a1bf7c10c9c43c0d6d87e2b3efb79980
DzidediSenaya <dzidedisenaya@gmail.com>
2023-03-02 07:01:05 -0800

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: buffer
+ * @src: string to be copied
+ * @n: number of bytes
+ * Return: a pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+	/* a is string length for src*/
+
+	int b = 0;
+	/*b is string length for dest*/
+
+	while (src[a++])
+		b++;
+
+	for (a = 0; src[a] && a < n; a++)
+		dest[a] = src[a];
+
+	for (a = b; a < n; a++)
+		dest[a] = '\0';
+
+	return (dest);
+}
