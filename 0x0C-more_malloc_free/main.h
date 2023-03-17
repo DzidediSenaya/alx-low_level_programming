e641020e4841adca1caa3a8bd607989a00e91ea2
DzidediSenaya <dzidedisenaya@gmail.com>
2023-03-16 20:02:31 -0700

    updated 2 files

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -8,4 +11,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif /* MAIN_H */
+#endif
