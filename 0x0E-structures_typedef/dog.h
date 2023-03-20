63aadaf9a6c8d643bf42b381b6f89d3aea9df8ea
DzidediSenaya <dzidedisenaya@gmail.com>
2023-03-20 14:20:29 -0700

    updated dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,12 +16,10 @@ struct dog
 };
 
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
-int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
