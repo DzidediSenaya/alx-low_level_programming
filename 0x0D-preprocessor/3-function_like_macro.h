272f2e1c2f609ddc597139e2d0778cc09aca2000
DzidediSenaya <dzidedisenaya@gmail.com>
2023-03-20 15:51:52 -0700

    updated task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-#define ABS(x) (x >= 0 ? (x) : (-x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 #endif
