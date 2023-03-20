9c0c3f7867234619c6d28439d71ff8c85d9c31f8
DzidediSenaya <dzidedisenaya@gmail.com>
2023-03-20 15:29:38 -0700

    updated task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,40 +1,69 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
 
 /**
-  * new_dog - creates a new dog
-  * @name: dog's name
-  * @age: dog's age
-  * @owner: dog's owner
-  * Return: NULL if the function fails
-  */
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: The string to copy
+ * Return: a pointer to the duplicated string, NULL if insufficient memory
+ * or if @str is NULL
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	ar = malloc(sizeof(char) * (i + 1));
+	if (ar == NULL)
+		return (NULL);
+	while (str[j])
+	{
+		ar[j] = str[j];
+		j++;
+	}
+	ar[j] = 0;
+	return (ar);
+}
+/**
+ * new_dog - creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: NULL if function fails
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int newdog, olddog, i;
-	dog_t *puppy;
+	dog_t *new;
+	char *ncpy;
+	char *ocpy;
 
-	newdog = olddog = 0;
-	while (name[newdog++])
-		;
-	while (owner[olddog++])
-		;
-	puppy = malloc(sizeof(dog_t));
-	if (puppy == NULL)
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
 		return (NULL);
 
-	puppy->name = malloc(newdog * sizeof(puppy->name));
-	if (puppy == NULL)
+	ncpy = _strdup(name);
+	if (!ncpy && name)
+	{
+		free(new);
 		return (NULL);
-	for (i = 0; i < newdog; i++)
-		puppy->name[i] = name[i];
-
-	puppy->age = age;
-
-	puppy->owner = malloc(olddog * sizeof(puppy->owner));
-	if (puppy == NULL)
+	}
+	ocpy = _strdup(owner);
+	if (!ocpy && owner)
+	{
+		free(ncpy);
+		free(new);
 		return (NULL);
-	for (i = 0; i < olddog; i++)
-		puppy->owner[i] = owner[i];
-	return (puppy);
+	}
+
+	new->name = ncpy;
+	new->age = age;
+	new->owner = ocpy;
+
+	return (new);
 }
