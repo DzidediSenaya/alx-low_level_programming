b9eaa5d31811e8a7784b50f4ac30435eb8d53311
DzidediSenaya <dzidedisenaya@gmail.com>
2023-03-22 14:31:51 -0700

    updated task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
@@ -46,13 +44,9 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
+
 
 /**
  * op_mod - Returns the remainder of the division
@@ -64,10 +58,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
