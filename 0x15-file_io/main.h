2dbb5f0f661d305cec73edf3112290eac4799d81
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-05-09 17:15:03 +0000

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,8 +10,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int open_files(char *src_file, char *dest_file);
-void copy_file_content(int fd_from, int fd_to, char *buffer, char *filename);
+int append_text_to_file(const char *filename, char *text_content);
 
 # endif
 
