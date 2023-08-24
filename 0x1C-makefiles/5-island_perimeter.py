b95adea8ac04fad8e22cc59481edd265f784ea92
DzidediSenaya <dzidedisenaya@gmail.com>
2023-08-23 17:56:12 -0700

    finished all tasks

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module contains the def island_perimeter(grid) function
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in grid.
+
+    Args:
+    grid (list of list of integers): The grid representing the island.
+
+    Returns:
+    int: The perimeter of the island.
+    """
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+    return perimeter
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void school_function() {
+    printf("This is a function from the school.c file.\n");
+}
