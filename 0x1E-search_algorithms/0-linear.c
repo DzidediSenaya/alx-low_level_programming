3344a64d089928176c84fb3bc5525b26581463f1
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-10-11 14:58:39 +0000

    betty 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,15 +13,15 @@ int linear_search(int *array, size_t size, int value)
 {
 size_t i;
 
-    if (array == NULL)
-        return (-1);
+if (array == NULL)
+return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (int)i;
-    }
+for (i = 0; i < size; i++)
+{
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (int)i;
+}
 
-    return (-1);  /*Value not found*/
+return (-1);  /*Value not found*/
 }
